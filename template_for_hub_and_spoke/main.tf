cb1b9cf7ae3843f90580c3ccdd8edf20e9b1f1ed
Seyi Abiodun <cypher04>
2025-05-22 12:40:18 +0100

    added tfvars file to store variables to edit them easily

--- template_for_hub_and_spoke/main.tf
+++ template_for_hub_and_spoke/main.tf
@@ -9,51 +9,6 @@ Includes:
 */
 
 // variables.tf
-variable "location" {
-  description = "Azure region for all resources"
-  type        = string
-  default     = "West Europe"
-}
-
-variable "resource_group_name" {
-  description = "Name of the Resource Group"
-  type        = string
-  default     = "rg-hub-spoke-demo"
-}
-
-variable "hub_vnet" {
-  description = "Settings for the hub virtual network"
-  type = object({
-    name          = string
-    address_space = list(string)
-    subnet_prefix = string
-  })
-  default = {
-    name          = "vnet-hub"
-    address_space = ["10.0.0.0/16"]
-    subnet_prefix = "10.0.1.0/24"
-  }
-}
-
-variable "spokes" {
-  description = "List of spoke VNets with name, address_space and subnet_prefix"
-  type = list(object({
-    name          = string
-    address_space = list(string)
-    subnet_prefix = string
-  }))
-  default = [
-    { name = "vnet-spoke-1", address_space = ["10.1.0.0/16"], subnet_prefix = "10.1.1.0/24" },
-    { name = "vnet-spoke-2", address_space = ["10.2.0.0/16"], subnet_prefix = "10.2.1.0/24" }
-  ]
-}
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default     = { environment = "demo" }
-}
-
 
 // main.tf
 terraform {
