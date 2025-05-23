1960d1d4bf1544bca4822727a60b783fabb0538e
Seyi Abiodun <cypher04>
2025-05-23 15:28:39 +0100

    cleanup of unused code

--- template_for_hub_and_spoke/main.tf
+++ template_for_hub_and_spoke/main.tf
@@ -156,57 +156,6 @@ resource "azurerm_network_security_rule" "deny_ssh" {
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.hub_nsg.name
 }
-
-    # security_rule {
-    # name                       = "ssh"
-    # priority                   = 100
-    # direction                  = "Inbound"
-    # access                     = "Allow"
-    # protocol                   = "Tcp"
-    # source_port_range          = "*"
-    # destination_port_range     = "22"
-    # source_address_prefix      = "*"
-    # destination_address_prefix = "*"
-    # }
-
-    # # security_rule {
-    # # name                       = "ssh"
-    # # priority                   = 200
-    # # direction                  = "Outbound"
-    # # access                     = "Allow"
-    # # protocol                   = "Tcp"
-    # # source_port_range          = "*"
-    # # destination_port_range     = "22"
-    # # source_address_prefix      = "*"
-    # # destination_address_prefix = "*"
-    # # }
-
-    # security_rule {
-    # name                       = "http"
-    # priority                   = 300
-    # direction                  = "Inbound"
-    # access                     = "Allow"
-    # protocol                   = "Tcp"
-    # source_port_range          = "*"
-    # destination_port_range     = "80"
-    # source_address_prefix      = "*"
-    # destination_address_prefix = "*"
-    # }
-
-    # security_rule {
-    # name                       = "http"
-    # priority                   = 300
-    # direction                  = "Outbound"
-    # access                     = "Deny"
-    # protocol                   = "Tcp"
-    # source_port_range          = "*"
-    # destination_port_range     = "80"
-    # source_address_prefix      = "*"
-    # destination_address_prefix = "*"
-    # }
-    # tags = {
-    # environment = "hub"
-    # }
   
 
 // attach nsg assocciation
