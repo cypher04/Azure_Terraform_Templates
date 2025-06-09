dc1f5f3a03e9852cf9b4e5d5051be32f55017f56
Seyi Abiodun <cypher04>
2025-06-09 02:13:50 +0100

    created hub and spoke modules for terraform reuse

---
+++ template_for_modules/modules/hub_and_spoke/main.tf
@@ -0,0 +1,53 @@
+// This is the main Terraform configuration file for the hub-and-spoke network topology.
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "hub" {
+  name     = "var.resource_group_name"
+  location = "var.location"
+}
+resource "azurerm_virtual_network" "hub" {
+  name                = "var.hub_vnet_name"
+  address_space       = ["var.hub_address_space"]
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+}
+resource "azurerm_subnet" "hub" {
+  name                 = "var.hub_subnet_name"
+  resource_group_name  = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["var.hub_subnet_prefix"]
+}
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+  name                      = "var.hub_to_spoke_peering_name"
+  resource_group_name       = azurerm_resource_group.hub.name
+  virtual_network_name      = azurerm_virtual_network.hub.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+    allow_gateway_transit        = true
+    use_remote_gateways          = false
+}
+resource "azurerm_virtual_network" "spoke" {
+  name                = "var.spoke_vnet_name"
+  address_space       = ["var.spoke_address_space"]   
+    location            = azurerm_resource_group.hub.location
+    resource_group_name = azurerm_resource_group.hub.name
+}
+resource "azurerm_subnet" "spoke" {
+    name                 = "var.spoke_subnet_name"   
+    resource_group_name  = azurerm_resource_group.hub.name
+    virtual_network_name = azurerm_virtual_network.spoke.name
+    address_prefixes     = ["var.spoke_subnet_prefix"]
+    }
+resource "azurerm_virtual_network_peering" "spoke_to_hub" {
+    name                      = "var.spoke_to_hub_peering_name"
+    resource_group_name       = azurerm_resource_group.hub.name
+    virtual_network_name      = azurerm_virtual_network.spoke.name
+    remote_virtual_network_id = azurerm_virtual_network.hub.id
+    allow_virtual_network_access = true
+    allow_forwarded_traffic      = true
+    allow_gateway_transit        = false
+    use_remote_gateways          = true
+}
+
